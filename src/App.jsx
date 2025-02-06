98d7924430f9107c42fdc5c6c9a58de702bf7c48
michalinapepel <misia.pepel@onet.pl>
2025-02-06 13:23:31 +0100

    Math Tool for EditorJS
    Stage with disappearing bug

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ export default function App() {
         list: List,
         math: MathTool,
       },
+      data: {
+        blocks: [{ type: "math", data: { math: "2*2=4" } }],
+      },
 
       onReady: () => {
         ejInstance.current = editor;
